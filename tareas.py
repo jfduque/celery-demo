6a8be7b0a3cf1677e783c302e9e2fe4ddde2bde8
Juan F. Duque G <jfduque@gmail.com>
2017-11-20 13:06:02 -0500

    Nueva tarea esperar()

--- tareas.py
+++ tareas.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import time
+
 from celery import Celery
 
 app = Celery('tareas', broker='redis://localhost/0')
@@ -6,4 +8,9 @@ app = Celery('tareas', broker='redis://localhost/0')
 @app.task
 def sumar(x, y):
     return x + y
-    
+
+@app.task
+def esperar():
+	for x in range(5):
+		print(x)
+		time.sleep(1)
